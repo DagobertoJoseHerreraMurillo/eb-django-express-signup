cfadc3a47fc7cb1222999b7c4fbfc72ba2ecbdd1
Sergio <sergio.jose.ruiz@est.fib.upc.edu>
2018-04-11 17:29:12 +0200

    commit

--- form/views.py
+++ form/views.py
@@ -6,12 +6,6 @@ from .models import Leads
 def home(request):
     return render(request, 'index.html')
 
-
-def signup(request):
-    leads = Leads()
-    status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
-    return HttpResponse('', status=status)
-
 def signup(request):
     leads = Leads()
     status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
